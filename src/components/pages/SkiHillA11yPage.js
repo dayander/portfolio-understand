f423ceea5f687f9df2de7a21e3aee46b6fdd3d1b
Anderson Day <dayander@msu.edu>
2019-05-29 13:45:20 -0400

    updated

--- src/components/pages/SkiHillA11yPage.js
+++ src/components/pages/SkiHillA11yPage.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import {Header} from "../Header";
 import {Row, Col, Well, Jumbotron, } from 'react-bootstrap';
+import {PullSection} from "../layout/layout";
+import {Picture} from "../layout/layout";
+import {PageTitle} from "../a11y/pageA11y";
+import {getHome, getOne, getProjects} from "../../actions/projectsActions";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
+import {ProjectLarge} from "../projects/ProjectLarge";
 
 
 class SkiHillA11yPage extends React.Component{
@@ -12,48 +19,144 @@ class SkiHillA11yPage extends React.Component{
 
     componentDidMount(){
         console.log(this.props.match.path)
+
+        document.getElementById('app').focus();
+        window.scrollTo(0, 0);
+
+
+
+        //const projectObj = this.props.projects.find(x => x.getter === this.props.params);
+
+        PageTitle("Ski Hill A11y" + " || Anderson Day");
     }
 
 
 
 
     render(){
+        const projectObj = this.props.projects.find(x => x.getter === this.props.params);
+
+        const proejctList = this.props.projects.map((project, i)=>{
+
+            if(project !== projectObj && i < 2){
+                return(
+
+
+                    <Col className={'container'} key={i} xs={12} sm={6} bsStyle='project-list'>
+                        {/*<BookItem  _id={project._id} title={project.title} images={project.images} description={project.description} price={project.price} />*/}
+                        <ProjectLarge companyName={project.companyName} projectHeading={project.projectHeading} getter={project.getter} to={project.getter} img={project.bgImage} styleClass={'small'} />
+                    </Col>
+
+
+                )
+            }
+        });
+
+
+        const ProjectPictureLarge = () =>{
+            return(
+                <Row>
+                    <div className="container">
+                        <Col xs={12}>
+                            <Picture src={'/images/skihill-a11y/challengeImage.jpg'} alt={''}/>
+
+                        </Col>
+                    </div>
+                </Row>
+            )
+        };
+
+
+
+
+
+
         return(
             <div>
-                <Header h1={'Ski Hill A11y'}/>
-                <Well className={'white'}>
+                <Header h1={'Ski Hill A11y'}  color={"#fff"} img={'/images/skihill-a11y/headerImage.jpg'}
+                    h2={'An App for Accessibility Education'}
+                />
+                <div className={'white custom-well'}>
                     <Jumbotron className={'project-overview'}>
                     <div className={'container'}>
                         <h2>Project Overview</h2>
 
 
+
+                                <h3>Project Description: </h3>
+                                <p>Ski hill a11y is a web application that has two copies of the site that
+                                    are easily toggled between. The site starts out in accessible and allows
+                                    the user to toggle to the accessible version. I built this as a tool for
+                                    learning web developers to see examples of what makes a site accessible versus
+                                    in accessible. The application contains errors with heading structure, language
+                                    of the web page, color contrast, image alternative text, form labels on inputs,
+                                    being able to increase text size, keyboard navigation, semantic lists and updating
+                                    page titles. This project was also presented at the Accessible Learning Confernce 2018,
+                                    "An Introduction to Web Accessibility."</p>
+
+
+
+                                <h3>Technologies Used: </h3>
+                                    <ul>
+                                        <li>React</li>
+                                        <li>Redux</li>
+                                        <li>Node and Express</li>
+                                        <li>WCAG 2.0</li>
+                                    </ul>
+
+
+
+
+
+                                <h3>Project Role: </h3>
+                                    <p> I designed and developed this application with input from Michigan State University
+                                        faculty aligning course goals with accessibility knowledge in the Experince Architecture's
+                                    program Introduction to Web Authoring and Advacned Web Authoring</p>
+
+                                <h3>Live Project Links </h3>
                         <ul>
                             <li>
-                                <span>Project Description: </span>
-
-
+                                <a href={"http://skihill-a11y.herokuapp.com/"} > Ski Hill A11y Live </a>
                             </li>
                             <li>
-                                <span>Technologies Used: </span>
-
-
+                                <a href={"https://docs.google.com/presentation/d/1SPBQfTHbIX4fvw4XtloGkpQK8mSqqygUKhpTAvL6zpA/edit?usp=sharing"} > Accessible Learning Conference Presentation Slides </a>
                             </li>
-
                             <li>
-                                <span>Project Role: </span>
-
-
+                                <a href={"https://github.com/dayander/Accessible-Learning-Confernce-2018-presentation-w-server"} > Ski Hill A11y Github Repo </a>
                             </li>
 
-                            <li>
-                                <span>Live Project Links </span>
-
-
-                            </li>
 
                         </ul>
+
+
+
                     </div>
                     </Jumbotron>
+                </div>
+
+
+
+
+
+                <PullSection  heading={'Challenge'} body={'During the summer of 2018 I went on a study away to California put on by Teach Access to learn about accessibility in the tech industry. During my week visiting Google, Facebook, Microsoft, Adobe and Verizon Media. My group a fellow student from Michigan state and two students looked for gaps in our course work where we could implement Accessibility into the learning.'}/>
+
+                <ProjectPictureLarge/>
+
+                <PullSection heading={'Process'} body={'With the goal of educating designers for the web, Ski Hill A11y provides a way for those learning HTML and CSS to see a real example of how small changes in code make a large difference in the overall accessibility of the site.'}/>
+
+
+                <PullSection heading={'Outcome'} body={'A web application for those learning basic web development and accessibility to see differnces in accessible and not accessible HTML and CSS.'}/>
+
+
+
+
+                <Well>
+                    <Row>
+                        <div className="container">
+                            <h2>Onward</h2>
+                        </div>
+                        {proejctList}
+                    </Row>
                 </Well>
 
             </div>
@@ -64,7 +167,25 @@ class SkiHillA11yPage extends React.Component{
 }
 
 
+const mapStateToProps = (state)=>{
+    return{
+
+        projects: state.projects,
+        projectName: state.projectName,
 
 
+    }
+};
 
-export default SkiHillA11yPage;
+const mapDispatchToProps= ( dispatch) =>{
+    return bindActionCreators(
+        {
+            getProjects,
+            getHome,
+            getOne,
+
+        }, dispatch)
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(SkiHillA11yPage);
